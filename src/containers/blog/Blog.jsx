9ceadf4c775b95c7423198edbcaf33259e624f92
Mert Can Akdoğan <mertcakdogan@gmail.com>
2023-03-13 18:09:41 +0300

    Containers dosyaları hazırlandı.

---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './blog.css';
+const Blog = () => {
+  return <div>
+    <h1>Blog</h1>
+  </div>;
+}
+
+export default Blog;
---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './header.css';
+const Header = () => {
+  return <div>
+    <h1>Header</h1>
+  </div>;
+}
+
+export default Header;
